112c0d351bdb445cdf5e2d3a0a36dcb91832f030
Damiano Martorelli, phD <martorelli.damiano@gmail.com>
2019-11-07 17:22:31 +0100

    MaudPreferences modify for taking care of null situations

--- src/it/unitn/ing/rista/util/MaudPreferences.java
+++ src/it/unitn/ing/rista/util/MaudPreferences.java
@@ -19,16 +19,18 @@
  */
 
 package it.unitn.ing.rista.util;
-import it.unitn.ing.rista.interfaces.PreferencesInterface;
 
 import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 
+import it.unitn.ing.rista.interfaces.PreferencesInterface;
 /**
- *  The MaudPreferences is a class to manage the Maud preferences.
+ * <p>
+ * This class manages Maud preferences.
+ * </p>
  *
- * @version $Revision: 1.6 $, $Date: 2006/01/19 14:45:59 $
- * @author Luca Lutterotti
+ * @version $Revision: 1.7 $, $Date: 2017/07/19 14:45:59 $
+ * @author Luca Lutterotti, revised by Damiano Martorelli
  * @since JDK1.1
  */
 
@@ -37,17 +39,24 @@ public class MaudPreferences extends PreferencesInterface {
 
 	public static Preferences prefs;
 
+  /**
+   * Class constructor.
+   */
   public MaudPreferences() {
   }
-
-	public static void resetPreferences() {
-		try {
-			prefs.clear();
-		} catch (BackingStoreException e) {
-			e.printStackTrace();
-		}
+  /**
+   * Resets MAUD preferences.
+   */
+  public static void resetPreferences() {
+	try {
+		prefs.clear();
+	} catch (BackingStoreException e) {
+		e.printStackTrace();
 	}
-
+  }
+  /**
+   * Load MAUD preferences.
+   */
   public static void loadPreferences() {
 	  prefs = Preferences.userRoot().node(MaudPreferences.class.getName());
   }
@@ -60,9 +69,22 @@ public class MaudPreferences extends PreferencesInterface {
     MaudPreferences.setPref(key, value.toString());
   }
 
-	public static boolean contains(String key) {
-		return prefs.get(key, null) != null;
-	}
+  /**
+   * Checks if the key exists.
+   * 
+   * @param key
+   *          the key to be checked.
+   * @return <code>true</code> if the key is defined, <code>false</code>
+   *         otherwise.
+   */
+  public static boolean contains(String key) {
+    if (prefs != null)
+    {
+      return prefs.get(key, null) != null;
+    }
+    else
+      return false;
+  }
 
   public static void setPref(String key, String value) {
     prefs.put(key, value);
@@ -83,39 +105,134 @@ public class MaudPreferences extends PreferencesInterface {
   public static void setPref(String key, boolean value) {
     prefs.putBoolean(key, value);
   }
-
-	public static String getPref(String key, String defaultValue) {
-		if (!contains(key))
-			prefs.put(key, defaultValue);
-		return prefs.get(key, defaultValue);
-	}
-
-	public static int getInteger(String key, int defaultValue) {
-	  if (!contains(key))
-		  prefs.putInt(key, defaultValue);
-	  return prefs.getInt(key, defaultValue);
+  
+  /**
+   * Gets value if a specified key is already mapped in the preferences,
+   * otherwise add it with the specified value.
+   * 
+   * @param key
+   *          the key to be retrieved.
+   * @param defaultValue
+   *          the value to be assigned is key is missing.
+   * @return the stored value if the key is defined, <code>defaultValue</code>
+   *         otherwise.
+   */
+  public static String getPref(String key, String defaultValue) {
+    if (prefs != null)
+    {
+      if (!contains(key))
+        prefs.put(key, defaultValue);
+      return prefs.get(key, defaultValue);
+    }
+    else
+      return defaultValue;    
+//	if (!contains(key))
+//		prefs.put(key, defaultValue);
+//	return prefs.get(key, defaultValue);
   }
-
+  
+  /**
+   * Gets value if a specified key is already mapped in the preferences,
+   * otherwise add it with the specified value.
+   * 
+   * @param key
+   *          the key to be retrieved.
+   * @param defaultValue
+   *          the value to be assigned is key is missing.
+   * @return the stored value if the key is defined, <code>defaultValue</code>
+   *         otherwise.
+   */
+  public static int getInteger(String key, int defaultValue) {
+    if (prefs != null)
+    {
+      if (!contains(key))
+        prefs.putInt(key, defaultValue);
+      return prefs.getInt(key, defaultValue);
+    }
+    else
+      return defaultValue;
+//	  if (!contains(key))
+//		  prefs.putInt(key, defaultValue);
+//	  return prefs.getInt(key, defaultValue);
+  }
+  
+  /**
+   * Gets value if a specified key is already mapped in the preferences,
+   * otherwise add it with the specified value.
+   * 
+   * @param key
+   *          the key to be retrieved.
+   * @param defaultValue
+   *          the value to be assigned is key is missing.
+   * @return the stored value if the key is defined, <code>defaultValue</code>
+   *         otherwise.
+   */
   public static double getDouble(String key, double defaultValue) {
-	  if (!contains(key))
-		  prefs.putDouble(key, defaultValue);
-    return prefs.getDouble(key, defaultValue);
+    if (prefs != null)
+    {
+      if (!contains(key))
+        prefs.putDouble(key, defaultValue);
+      return prefs.getDouble(key, defaultValue);
+    }
+    else
+      return defaultValue;    
+//	  if (!contains(key))
+//		  prefs.putDouble(key, defaultValue);
+//    return prefs.getDouble(key, defaultValue);
   }
-
-	public static long getLong(String key, long defaultValue) {
-		if (!contains(key))
-			prefs.putLong(key, defaultValue);
-		return prefs.getLong(key, defaultValue);
-	}
+  
+  /**
+   * Gets value if a specified key is already mapped in the preferences,
+   * otherwise add it with the specified value.
+   * 
+   * @param key
+   *          the key to be retrieved.
+   * @param defaultValue
+   *          the value to be assigned is key is missing.
+   * @return the stored value if the key is defined, <code>defaultValue</code>
+   *         otherwise.
+   */
+  public static long getLong(String key, long defaultValue) {
+    if (prefs != null)
+    {
+      if (!contains(key))
+        prefs.putLong(key, defaultValue);
+      return prefs.getLong(key, defaultValue);
+    }
+    else
+      return defaultValue;       
+//		if (!contains(key))
+//			prefs.putLong(key, defaultValue);
+//		return prefs.getLong(key, defaultValue);
+  }
 
 	public static boolean getBoolean(String key) {
 		return getBoolean(key, false);
   }
 
-  public static boolean getBoolean(String key, boolean defaultValue) {
-	  if (!contains(key))
-		  prefs.putBoolean(key, defaultValue);
-	  return prefs.getBoolean(key, defaultValue);
+  /**
+   * Gets value if a specified key is already mapped in the preferences,
+   * otherwise add it with the specified value.
+   * 
+   * @param key
+   *          the key to be retrieved.
+   * @param defaultValue
+   *          the value to be assigned is key is missing.
+   * @return <code>true</code> if the key is defined, <code>false</code>
+   *         otherwise.
+   */
+	public static boolean getBoolean(String key, boolean defaultValue) {
+      if (prefs != null)
+	  {
+	    if (!contains(key))
+	      prefs.putBoolean(key, defaultValue);
+	    return prefs.getBoolean(key, defaultValue);
+	  }
+	  else
+	    return defaultValue;  	  
+//	  if (!contains(key))
+//		  prefs.putBoolean(key, defaultValue);
+//	  return prefs.getBoolean(key, defaultValue);
   }
 
 }
