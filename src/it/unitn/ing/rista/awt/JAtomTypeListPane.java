1ac1c11f83f3eef78028d4ad75bda661f0e5558e
Damiano Martorelli, phD <damianus.m@gmail.com>
2019-11-08 08:11:47 +0100

    imports update

--- src/it/unitn/ing/rista/awt/JAtomTypeListPane.java
+++ src/it/unitn/ing/rista/awt/JAtomTypeListPane.java
@@ -3,31 +3,36 @@
  *
  * Copyright (c) 1996-2016 Luca Lutterotti All Rights Reserved.
  *
- * This software is the research result of the author and it is 
- * provided as it is as confidential and proprietary information.  
- * You shall not disclose such Confidential Information and shall use 
- * it only in accordance with the terms of the license agreement you 
- * entered into with the author.
+ * This software is the research result of the author and it is provided as it
+ * is as confidential and proprietary information. You shall not disclose such
+ * Confidential Information and shall use it only in accordance with the terms
+ * of the license agreement you entered into with the author.
  *
- * THE AUTHOR MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE
- * SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
- * PURPOSE, OR NON-INFRINGEMENT. THE AUTHOR SHALL NOT BE LIABLE FOR ANY DAMAGES
- * SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING
- * THIS SOFTWARE OR ITS DERIVATIVES.
+ * THE AUTHOR MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF
+ * THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR
+ * NON-INFRINGEMENT. THE AUTHOR SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY
+ * LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS
+ * DERIVATIVES.
  *
  */
 
 package it.unitn.ing.rista.awt;
 
-import it.unitn.ing.rista.chemistry.XRayDataSqLite;
-import it.unitn.ing.rista.diffr.*;
-import it.unitn.ing.rista.util.Constants;
-import it.unitn.ing.rista.util.MoreMath;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import it.unitn.ing.rista.chemistry.XRayDataSqLite;
+import it.unitn.ing.rista.diffr.AtomScatterer;
+import it.unitn.ing.rista.diffr.Parameter;
+import it.unitn.ing.rista.util.MoreMath;
 
 /**
  * The JAtomTypeListPane is a class ....
@@ -39,11 +44,10 @@ import java.awt.event.*;
 
 public class JAtomTypeListPane extends JSubordListPane {
 
-  
   /**
    * Serial ID
    */
-  private static final long serialVersionUID  = -5622779308651768257L;
+  private static final long serialVersionUID = -5622779308651768257L;
   /**
    * Defines the button for selecting atom from periodic table.
    */
@@ -58,143 +62,141 @@ public class JAtomTypeListPane extends JSubordListPane {
    *          is the flag which commands if all is shown.
    */
   public JAtomTypeListPane(Frame parent, boolean showTotal) {
-	super(parent, showTotal);
+    super(parent, showTotal);
   }
+
   /**
    * 
    */
   @Override
   public void addCustomControlsToFieldsPanel() {
-	JPanel pnlAtomButtons = new JPanel(new FlowLayout());
+    JPanel pnlAtomButtons = new JPanel(new FlowLayout());
 
-	this.pnlCenter.add(pnlAtomButtons, BorderLayout.NORTH);
-	this.btnAtomTypeChoice = new JIconButton("PeriodicTable.gif");
+    this.pnlCenter.add(pnlAtomButtons, BorderLayout.NORTH);
+    this.btnAtomTypeChoice = new JIconButton("PeriodicTable.gif");
 
-	pnlAtomButtons.add(new JLabel("Atom type:"));
-	pnlAtomButtons.add(btnAtomTypeChoice);
-	btnAtomTypeChoice.setToolTipText("Press to select the atom type");
-	btnAtomTypeChoice.addActionListener(new ActionListener() {
-	  public void actionPerformed(ActionEvent event) {
-		chooseTheAtom();
-	  }
-	});
-	//
-	JButton infoButton = new JButton("Info");
-	infoButton.addActionListener(new ActionListener() {
-	  public void actionPerformed(ActionEvent event) {
-		showInfoPanel();
-	  }
-	});
+    pnlAtomButtons.add(new JLabel("Atom type:"));
+    pnlAtomButtons.add(btnAtomTypeChoice);
+    btnAtomTypeChoice.setToolTipText("Press to select the atom type");
+    btnAtomTypeChoice.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent event) {
+        chooseTheAtom();
+      }
+    });
+    //
+    JButton infoButton = new JButton("Info");
+    infoButton.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent event) {
+        showInfoPanel();
+      }
+    });
 
-	pnlAtomButtons.add(infoButton);
+    pnlAtomButtons.add(infoButton);
   }
-  
+
   /**
    * Sets the parameters for atom.
    */
   public void setParameterList() {
-	if (itsparent != null) {
-	  AtomScatterer scatterer = (AtomScatterer) itsparent.subordinateloopField[theindex].selectedElement();
-		if (scatterer != null && scatterer != selectedObject) {
-		  selectedObject = scatterer;
-		  for (int i = 0; i < fieldNumber; i++) {
-			Parameter apar = scatterer.parameterField[i];
-			if (apar != null) {
-			  ((myJFrame) getFrameParent()).removeComponentfromlist(valueTF[i]);
-//					System.out.println("Adding jatom: " + i + " " + valueTF[i].getText());
-			  ((myJFrame) getFrameParent()).addComponenttolist(valueTF[i], apar);
-			  valueTF[i].setText(apar.getValue());
-			}/* else {
-//						System.out.println("Removing jatom: " + i + " " + valueTF[i].getText());
-						((myJFrame) getFrameParent()).removeComponentfromlist(valueTF[i]);
-					}*/
-		  }
-		  btnAtomTypeChoice.setText(scatterer.getAtomSymbol());
-		}
-	  }
-	}
-  
-	/**
-	 * Activates the periodic table for atom selection.
-	 */
-	protected void chooseTheAtom() {
-		AtomScatterer scatterer = null;
-		if (itsparent != null) {
-			scatterer = (AtomScatterer) itsparent.subordinateloopField[theindex].selectedElement();
-			if (scatterer != null)
-				ChooseAtomD.getAtomType(getFrameParent(), scatterer);
-			btnAtomTypeChoice.setText(scatterer.getAtomSymbol());
-		}
-	}
-	  
-	/**
-	 * Shows the info panel with plots of absorption (total and photo), scattering
-	 * (coherent and incoherent).
-	 */
-	public void showInfoPanel() {
-		AtomScatterer scatterer = (AtomScatterer) itsparent.subordinateloopField[theindex].selectedElement();
-		if (scatterer == null)
-			return;
-		int atomicNumber = scatterer.getAtomicNumber();
+    if (itsparent != null) {
+      AtomScatterer scatterer = (AtomScatterer) itsparent.subordinateloopField[theindex].selectedElement();
+      if (scatterer != null && scatterer != selectedObject) {
+        selectedObject = scatterer;
+        for (int i = 0; i < fieldNumber; i++) {
+          Parameter apar = scatterer.parameterField[i];
+          if (apar != null) {
+            ((myJFrame) getFrameParent()).removeComponentfromlist(valueTF[i]);
+            // System.out.println("Adding jatom: " + i + " " +
+            // valueTF[i].getText());
+            ((myJFrame) getFrameParent()).addComponenttolist(valueTF[i], apar);
+            valueTF[i].setText(apar.getValue());
+          } /*
+             * else { // System.out.println("Removing jatom: " + i + " " +
+             * valueTF[i].getText()); ((myJFrame)
+             * getFrameParent()).removeComponentfromlist(valueTF[i]); }
+             */
+        }
+        btnAtomTypeChoice.setText(scatterer.getAtomSymbol());
+      }
+    }
+  }
 
-		int plotCounts = 3000;
-		double[] x = new double[plotCounts];
+  /**
+   * Activates the periodic table for atom selection.
+   */
+  protected void chooseTheAtom() {
+    AtomScatterer scatterer = null;
+    if (itsparent != null) {
+      scatterer = (AtomScatterer) itsparent.subordinateloopField[theindex].selectedElement();
+      if (scatterer != null)
+        ChooseAtomD.getAtomType(getFrameParent(), scatterer);
+      btnAtomTypeChoice.setText(scatterer.getAtomSymbol());
+    }
+  }
 
-		double[] y = new double[plotCounts];
-		double xstart = 1.01;
-		double xstep = 0.01;
-		for (int i = 0; i < plotCounts; i++) {
-			x[i] = xstart + i * xstep;
-			y[i] = XRayDataSqLite.getTotalAbsorptionForAtomAndEnergy(atomicNumber, x[i]);
-			if (y[i] > 0)
-				y[i] = MoreMath.log10(y[i]);
-			else
-				y[i] = 0;
-		}
-		(new PlotSimpleData(getFrameParent(), x, y)).setVisible(true);
+  /**
+   * Shows the info panel with plots of absorption (total and photo), scattering
+   * (coherent and incoherent).
+   */
+  public void showInfoPanel() {
+    AtomScatterer scatterer = (AtomScatterer) itsparent.subordinateloopField[theindex].selectedElement();
+    if (scatterer == null)
+      return;
+    int atomicNumber = scatterer.getAtomicNumber();
 
-		for (int i = 0; i < plotCounts; i++) {
-			x[i] = xstart + i * xstep;
-			y[i] = XRayDataSqLite.getCoherentScatteringForAtomAndEnergy(atomicNumber, x[i]);
-			if (y[i] > 0)
-				y[i] = MoreMath.log10(y[i]);
-			else
-				y[i] = 0;
-		}
-		(new PlotSimpleData(getFrameParent(), x, y)).setVisible(true);
+    int plotCounts = 3000;
+    double[] x = new double[plotCounts];
 
-		for (int i = 0; i < plotCounts; i++) {
-			x[i] = xstart + i * xstep;
-			y[i] = XRayDataSqLite.getIncoherentScatteringForAtomAndEnergy(atomicNumber, x[i]);
-			if (y[i] > 0)
-				y[i] = MoreMath.log10(y[i]);
-			else
-				y[i] = 0;
-		}
-		(new PlotSimpleData(getFrameParent(), x, y)).setVisible(true);
+    double[] y = new double[plotCounts];
+    double xstart = 1.01;
+    double xstep = 0.01;
+    for (int i = 0; i < plotCounts; i++) {
+      x[i] = xstart + i * xstep;
+      y[i] = XRayDataSqLite.getTotalAbsorptionForAtomAndEnergy(atomicNumber, x[i]);
+      if (y[i] > 0)
+        y[i] = MoreMath.log10(y[i]);
+      else
+        y[i] = 0;
+    }
+    (new PlotSimpleData(getFrameParent(), x, y)).setVisible(true);
 
-		for (int i = 0; i < plotCounts; i++) {
-			x[i] = xstart + i * xstep;
-			y[i] = XRayDataSqLite.getPhotoAbsorptionForAtomAndEnergy(atomicNumber, x[i]);
-			if (y[i] > 0)
-				y[i] = MoreMath.log10(y[i]);
-			else
-				y[i] = 0;
-		}
-		(new PlotSimpleData(getFrameParent(), x, y)).setVisible(true);
+    for (int i = 0; i < plotCounts; i++) {
+      x[i] = xstart + i * xstep;
+      y[i] = XRayDataSqLite.getCoherentScatteringForAtomAndEnergy(atomicNumber, x[i]);
+      if (y[i] > 0)
+        y[i] = MoreMath.log10(y[i]);
+      else
+        y[i] = 0;
+    }
+    (new PlotSimpleData(getFrameParent(), x, y)).setVisible(true);
 
-/*		for (int i = 0; i < plotCounts; i++) {
-			x[i] = xstart + i * xstep;
-			y[i] = XRayDataSqLite.getPhotoAbsorptionForAtomAndEnergyDiv(atomicNumber, x[i]);
-			if (y[i] > 0)
-				y[i] = MoreMath.log10(y[i]);
-			else
-				y[i] = 0;
-		}
-		(new PlotSimpleData(getFrameParent(), x, y)).setVisible(true);*/
+    for (int i = 0; i < plotCounts; i++) {
+      x[i] = xstart + i * xstep;
+      y[i] = XRayDataSqLite.getIncoherentScatteringForAtomAndEnergy(atomicNumber, x[i]);
+      if (y[i] > 0)
+        y[i] = MoreMath.log10(y[i]);
+      else
+        y[i] = 0;
+    }
+    (new PlotSimpleData(getFrameParent(), x, y)).setVisible(true);
 
+    for (int i = 0; i < plotCounts; i++) {
+      x[i] = xstart + i * xstep;
+      y[i] = XRayDataSqLite.getPhotoAbsorptionForAtomAndEnergy(atomicNumber, x[i]);
+      if (y[i] > 0)
+        y[i] = MoreMath.log10(y[i]);
+      else
+        y[i] = 0;
+    }
+    (new PlotSimpleData(getFrameParent(), x, y)).setVisible(true);
 
-	}
+    /*
+     * for (int i = 0; i < plotCounts; i++) { x[i] = xstart + i * xstep; y[i] =
+     * XRayDataSqLite.getPhotoAbsorptionForAtomAndEnergyDiv(atomicNumber, x[i]);
+     * if (y[i] > 0) y[i] = MoreMath.log10(y[i]); else y[i] = 0; } (new
+     * PlotSimpleData(getFrameParent(), x, y)).setVisible(true);
+     */
 
+  }
 
 }
