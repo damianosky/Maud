3cb4465488ce6d9ae49736ab8d617cda0a5c55bc
Damiano Martorelli, phD <damianus.m@gmail.com>
2019-11-08 08:21:52 +0100

    code formatting update

--- src/it/unitn/ing/rista/util/MaudPreferences.java
+++ src/it/unitn/ing/rista/util/MaudPreferences.java
@@ -3,18 +3,17 @@
  *
  * Copyright (c) 2000 Luca Lutterotti All Rights Reserved.
  *
- * This software is the research result of Luca Lutterotti and it is
- * provided as it is as confidential and proprietary information.
- * You shall not disclose such Confidential Information and shall use
- * it only in accordance with the terms of the license agreement you
- * entered into with Luca Lutterotti.
+ * This software is the research result of Luca Lutterotti and it is provided as
+ * it is as confidential and proprietary information. You shall not disclose
+ * such Confidential Information and shall use it only in accordance with the
+ * terms of the license agreement you entered into with Luca Lutterotti.
  *
- * THE AUTHOR MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF THE
- * SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
- * PURPOSE, OR NON-INFRINGEMENT. THE AUTHOR SHALL NOT BE LIABLE FOR ANY DAMAGES
- * SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING
- * THIS SOFTWARE OR ITS DERIVATIVES.
+ * THE AUTHOR MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF
+ * THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR
+ * NON-INFRINGEMENT. THE AUTHOR SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY
+ * LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING THIS SOFTWARE OR ITS
+ * DERIVATIVES.
  *
  */
 
@@ -24,6 +23,7 @@ import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 
 import it.unitn.ing.rista.interfaces.PreferencesInterface;
+
 /**
  * <p>
  * This class manages Maud preferences.
@@ -34,31 +34,33 @@ import it.unitn.ing.rista.interfaces.PreferencesInterface;
  * @since JDK1.1
  */
 
-
 public class MaudPreferences extends PreferencesInterface {
 
-	public static Preferences prefs;
+  public static Preferences prefs;
 
   /**
    * Class constructor.
    */
   public MaudPreferences() {
   }
+
   /**
    * Resets MAUD preferences.
    */
   public static void resetPreferences() {
-	try {
-		prefs.clear();
-	} catch (BackingStoreException e) {
-		e.printStackTrace();
-	}
+    try {
+      prefs.clear();
+    }
+    catch (BackingStoreException e) {
+      e.printStackTrace();
+    }
   }
+
   /**
    * Load MAUD preferences.
    */
   public static void loadPreferences() {
-	  prefs = Preferences.userRoot().node(MaudPreferences.class.getName());
+    prefs = Preferences.userRoot().node(MaudPreferences.class.getName());
   }
 
   public Object getValue(String key) {
@@ -78,8 +80,7 @@ public class MaudPreferences extends PreferencesInterface {
    *         otherwise.
    */
   public static boolean contains(String key) {
-    if (prefs != null)
-    {
+    if (prefs != null) {
       return prefs.get(key, null) != null;
     }
     else
@@ -94,18 +95,18 @@ public class MaudPreferences extends PreferencesInterface {
     prefs.putInt(key, intvalue);
   }
 
-	public static void setPref(String key, long longvalue) {
-		prefs.putLong(key, longvalue);
-	}
+  public static void setPref(String key, long longvalue) {
+    prefs.putLong(key, longvalue);
+  }
 
-	public static void setPref(String key, double value) {
+  public static void setPref(String key, double value) {
     prefs.putDouble(key, value);
   }
 
   public static void setPref(String key, boolean value) {
     prefs.putBoolean(key, value);
   }
-  
+
   /**
    * Gets value if a specified key is already mapped in the preferences,
    * otherwise add it with the specified value.
@@ -118,19 +119,18 @@ public class MaudPreferences extends PreferencesInterface {
    *         otherwise.
    */
   public static String getPref(String key, String defaultValue) {
-    if (prefs != null)
-    {
+    if (prefs != null) {
       if (!contains(key))
         prefs.put(key, defaultValue);
       return prefs.get(key, defaultValue);
     }
     else
-      return defaultValue;    
-//	if (!contains(key))
-//		prefs.put(key, defaultValue);
-//	return prefs.get(key, defaultValue);
+      return defaultValue;
+    // if (!contains(key))
+    // prefs.put(key, defaultValue);
+    // return prefs.get(key, defaultValue);
   }
-  
+
   /**
    * Gets value if a specified key is already mapped in the preferences,
    * otherwise add it with the specified value.
@@ -143,19 +143,18 @@ public class MaudPreferences extends PreferencesInterface {
    *         otherwise.
    */
   public static int getInteger(String key, int defaultValue) {
-    if (prefs != null)
-    {
+    if (prefs != null) {
       if (!contains(key))
         prefs.putInt(key, defaultValue);
       return prefs.getInt(key, defaultValue);
     }
     else
       return defaultValue;
-//	  if (!contains(key))
-//		  prefs.putInt(key, defaultValue);
-//	  return prefs.getInt(key, defaultValue);
+    // if (!contains(key))
+    // prefs.putInt(key, defaultValue);
+    // return prefs.getInt(key, defaultValue);
   }
-  
+
   /**
    * Gets value if a specified key is already mapped in the preferences,
    * otherwise add it with the specified value.
@@ -168,19 +167,18 @@ public class MaudPreferences extends PreferencesInterface {
    *         otherwise.
    */
   public static double getDouble(String key, double defaultValue) {
-    if (prefs != null)
-    {
+    if (prefs != null) {
       if (!contains(key))
         prefs.putDouble(key, defaultValue);
       return prefs.getDouble(key, defaultValue);
     }
     else
-      return defaultValue;    
-//	  if (!contains(key))
-//		  prefs.putDouble(key, defaultValue);
-//    return prefs.getDouble(key, defaultValue);
+      return defaultValue;
+    // if (!contains(key))
+    // prefs.putDouble(key, defaultValue);
+    // return prefs.getDouble(key, defaultValue);
   }
-  
+
   /**
    * Gets value if a specified key is already mapped in the preferences,
    * otherwise add it with the specified value.
@@ -193,21 +191,20 @@ public class MaudPreferences extends PreferencesInterface {
    *         otherwise.
    */
   public static long getLong(String key, long defaultValue) {
-    if (prefs != null)
-    {
+    if (prefs != null) {
       if (!contains(key))
         prefs.putLong(key, defaultValue);
       return prefs.getLong(key, defaultValue);
     }
     else
-      return defaultValue;       
-//		if (!contains(key))
-//			prefs.putLong(key, defaultValue);
-//		return prefs.getLong(key, defaultValue);
+      return defaultValue;
+    // if (!contains(key))
+    // prefs.putLong(key, defaultValue);
+    // return prefs.getLong(key, defaultValue);
   }
 
-	public static boolean getBoolean(String key) {
-		return getBoolean(key, false);
+  public static boolean getBoolean(String key) {
+    return getBoolean(key, false);
   }
 
   /**
@@ -221,18 +218,17 @@ public class MaudPreferences extends PreferencesInterface {
    * @return <code>true</code> if the key is defined, <code>false</code>
    *         otherwise.
    */
-	public static boolean getBoolean(String key, boolean defaultValue) {
-      if (prefs != null)
-	  {
-	    if (!contains(key))
-	      prefs.putBoolean(key, defaultValue);
-	    return prefs.getBoolean(key, defaultValue);
-	  }
-	  else
-	    return defaultValue;  	  
-//	  if (!contains(key))
-//		  prefs.putBoolean(key, defaultValue);
-//	  return prefs.getBoolean(key, defaultValue);
+  public static boolean getBoolean(String key, boolean defaultValue) {
+    if (prefs != null) {
+      if (!contains(key))
+        prefs.putBoolean(key, defaultValue);
+      return prefs.getBoolean(key, defaultValue);
+    }
+    else
+      return defaultValue;
+    // if (!contains(key))
+    // prefs.putBoolean(key, defaultValue);
+    // return prefs.getBoolean(key, defaultValue);
   }
 
 }
